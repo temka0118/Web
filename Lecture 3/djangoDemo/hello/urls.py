16573b4f504781660f8d70952b5b7f65f4a1cf72
Temuujin <temuujintemka@gmail.com>
2022-04-02 16:17:13 +0800

    Lecture 3 added

---
+++ Lecture 3/djangoDemo/hello/urls.py
@@ -0,0 +1,9 @@
+import imp
+from unicodedata import name
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("<str:name>", views.greet, name="greet")
+]
